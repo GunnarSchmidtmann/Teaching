67a6a9e4d8bf5469fe767030ea5b0ec642d5f72f
Gunnar Schmidtmann <36344135+GunnarSchmidtmann@users.noreply.github.com>
2020-11-18 16:21:05 +0000

    Update Python_demo_show_size_analysis.py

--- Python_demo_show_size_analysis.py
+++ Python_demo_show_size_analysis.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 """
 Created on Fri Oct 30 10:50:30 2020
 
@@ -12,7 +10,6 @@ import numpy as np
 import seaborn as sns
 from scipy.stats import norm
 
-plt.close('all')
 
 
 df = pd.read_csv('Data/shoe_size1.csv', usecols=['Sex','Shoe Size.1'])
@@ -22,29 +19,45 @@ male = df.loc[df['Sex'] == 1]
 female = df.loc[df['Sex'] == 2]
 
 
-fig, (plt1, plt2, plt3) = plt.subplots(nrows=3, ncols=1)
 
-plt1.plot(male['Shoe Size.1'],'og',label='male')
+plt.figure(1)
+plt.plot(male['Shoe Size.1'],'og',label='male')
+plt.plot(female['Shoe Size.1'],'or',label='female')
+plt.legend()
+plt.xlabel('Subjects',fontsize=20)
+plt.ylabel('Shoe Size',fontsize=20)
+plt.grid(True)
+plt.savefig('scatter.eps', format='eps')
+plt.show() 
 
-plt1.plot(female['Shoe Size.1'],'or',label='female')
-plt1.legend()
-plt1.grid()
-
-# stats
 mean_male, std_male=norm.fit(male['Shoe Size.1'])
 mean_female, std_female=norm.fit(female['Shoe Size.1'])
 
 
+plt.figure(2)
+plt.hist(male['Shoe Size.1'],color = "g",normed=True,bins=10, alpha=0.5, label="male")
+plt.hist(female['Shoe Size.1'],color = "r",normed=True, bins=10, alpha=0.5, label="female")
+plt.xlabel('Shoe Size',fontsize=20)
+plt.ylabel('Frequency',fontsize=20)
+plt.xlim(0, 15)
+plt.grid(True)
+plt.savefig('histogram.eps', format='eps')
+plt.show() 
 
-plt2.hist(male['Shoe Size.1'],color = "g",normed=True,bins=10, alpha=0.5, label="male")
-plt2.hist(female['Shoe Size.1'],color = "r",normed=True, bins=10, alpha=0.5, label="female")
-plt2.invert_xaxis()
+plt.figure(3)
+sns.set_style("whitegrid")
+sns.distplot(male['Shoe Size.1'],fit=norm,bins=10,kde=False,color='g')
+sns.distplot(female['Shoe Size.1'],fit=norm,bins=10,kde=False,color='r')
+plt.xlabel('Shoe Size',fontsize=20)
+plt.ylabel('Frequency',fontsize=20)
+plt.xlim(0, 15)
+plt.savefig('histfit.eps')
+plt.show()
 
 
-# Plot the PDF.
-sns.set_color_codes()
-sns.distplot(male['Shoe Size.1'],fit=norm,bins=9,kde=False,color='g')
-sns.distplot(female['Shoe Size.1'],fit=norm,bins=9,kde=False,color='r')
+
+
+
 
 
 
