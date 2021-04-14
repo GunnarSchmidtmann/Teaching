6c853356a977c5a1a1a1c09e545252595d744ac6
Gunnar Schmidtmann <36344135+GunnarSchmidtmann@users.noreply.github.com>
2021-04-14 19:52:28 +0100

    Update Matlab_demo_shoe_size_analysis.m

--- Matlab_demo_shoe_size_analysis.m
+++ Matlab_demo_shoe_size_analysis.m
@@ -1,4 +1,4 @@
-%%% Gunnar Schmidtmann, University of Plymouth, November 2020
+%%% Gunnar Schmidtmann, University of Plymouth, April 2021
 
 clearvars
 close all
@@ -6,7 +6,7 @@ clc
 commandwindow
 
 
-%%% create a sepearte Data folder whcih should contina the 'shoe_size.xlsx' data file
+%%% create a sepearte data folder whcih should contain the 'shoe_size.xlsx' data file
 
 
 %%% set directories and read the data
@@ -16,6 +16,10 @@ cd(data_folder);
 
 filename = 'shoe_size.xlsx';
 [data,txt,raw] = xlsread(filename);
+
+%%% sort data with respect to column 1 (we need that later)
+sorted_data = (sortrows(data,[1 2]));
+
 
 %%% sort the data with respect to sex (male, female)
 for i = 1:length(data)
@@ -47,15 +51,16 @@ ylabel('Shoe Size')
 grid on
 ylim([0 14])
 
-%%% plot histograms, fit a Gaussian
+%%% plot histograms, fit a Gaussian ('normal') or another function (see
+%%% histfit)
 subplot(1,2,2) % histogram
-h=histfit(male_data(:,2),10,'normal')
+h=histfit(male_data(:,2),10,'kernel')
 h(2).Color = [0 0 1];
 h(1).FaceColor = [0 0 0.5]
 h(1).FaceAlpha = 0.5;
 
 hold on
-j=histfit(female_data(:,2),10,'normal')
+j=histfit(female_data(:,2),10,'kernel')
 j(2).Color = [1 0 0];
 j(1).FaceColor = [0.5 0 0];
 j(1).FaceAlpha = 0.5;
@@ -64,3 +69,41 @@ grid on
 set(gca,'FontSize',15)
 set (gca,'Xdir','reverse')
 camroll(270)
+
+%%% create box plots
+figure(2)
+subplot(1,2,1)
+C = [male_data(:,2) ; female_data(:,2)]; 
+grp = [zeros(1,length(male_data)),ones(1,length(female_data))];
+boxplot(C,grp,'Notch','on','Labels',{'Male','Female'})
+ylabel('Shoe Size');
+ylim([0 15])
+axis square
+set(gca, 'fontsize',20)
+set(gca, 'linewidth',1)
+title('Box Plots')
+grid on
+
+subplot(1,2,2)
+groups = cell(length(data),1);
+
+%%% replace 1s with 'Male' and 2s with 'Female'
+for i=1:length(sorted_data)
+    if sorted_data(i) == 1
+        groups(i) = {'Male'};
+    elseif sorted_data(i) == 2
+        groups(i) = {'Female'};
+    end
+end
+
+%%% create violin plots
+cd(current_dir);
+grouporder={'Male','Female'};
+vs = violinplot(sorted_data(:,2), groups,'GroupOrder',grouporder);
+ylabel('Shoe Size');
+ylim([0 15])
+axis square
+set(gca, 'fontsize',20)
+set(gca, 'linewidth',1)
+title('Violin Plots')
+grid on
